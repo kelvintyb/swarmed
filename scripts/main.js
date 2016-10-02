8dd248db0188b191d3ae515c6480b47e2bb260a5
Kelvin Tan <kelvin.tyb@gmail.com>
2016-10-02 22:59:04 +0800

    v1 map & player/enemy objects

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,44 @@
 $('document').ready(function() {
-  //  Note that this html file is set to pull down Phaser 2.5.0 from the JS Delivr CDN.
-  //  Although it will work fine with this tutorial, it's almost certainly not the most current version.
-  //  Be sure to replace it with an updated version before you start experimenting with adding your own code.
 
   var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameCanvas', {
     preload: preload,
     create: create,
-    // update: update
+    update: update
   });
+  //init global vars to be assigned game objects later
+  var enemies, player;
 
   function preload() {
-    console.log('CREATING IMAGEEEEE')
-    game.load.image('logo', 'assets/phaser.png');
+    //load images for background, player & enemies
+    game.load.image('sky', 'assets/sky.png');
+    game.load.image('enemy', 'assets/star.png')
+    game.load.spritesheet('player', 'assets/dude.png', 32, 48);
   }
 
   function create() {
+    //initialise physics system
+    game.physics.startSystem(Phaser.Physics.ARCADE)
+      //initialise game map background
+    game.add.sprite(0, 0, 'sky');
 
-    var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-    logo.anchor.setTo(0.5, 0.5);
+    //assign GLOBAL group to delegate control of enemies
+    enemies = game.add.group();
+    //enable physics for anything in enemies group
+    enemies.enableBody = true;
+    //create enemy and store into enemy var
+    var enemy = enemies.create(game.world.randomX, game.world.randomY, 'enemy');
 
+    //create player and assign to GLOBAL var
+    player = game.add.sprite(game.world.randomX, game.world.randomY, 'player');
+    console.log(player);
+    //enable physics
+    game.physics.arcade.enable(player);
+    player.body.collideWorldBounds = true;
+
+  }
+
+  function update() {
+    game.physics.arcade.collide(player, enemies);
   }
 
 });
