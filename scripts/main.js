3bf734775b2ddb6d2e0b3c88f1c4fc011a5f1995
Kelvin Tan <kelvin.tyb@gmail.com>
2016-10-03 09:56:47 +0800

    added collision handler and score counter

--- scripts/main.js
+++ scripts/main.js
@@ -3,10 +3,11 @@ $('document').ready(function() {
   var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameCanvas', {
     preload: preload,
     create: create,
-    update: update
+    update: update,
+    render: render
   });
-  //init global vars to be assigned game objects later
-  var enemies, enemy, player, control;
+  //init global vars to be assigned game objects later, consider refactoring later into global game object
+  var enemies, enemy, player, control, timer, score = 0;
 
   function preload() {
     //load images for background, player & enemies
@@ -27,10 +28,13 @@ $('document').ready(function() {
     enemies.enableBody = true;
     //create enemy and store into enemy var
     enemy = enemies.create(game.world.randomX, game.world.randomY, 'enemy');
+    //BUG: if using scale, may break overlap function later - cld workaround using .sprite on the player object and individual enemy objects
+    // enemy.scale.setTo(0.5, 0.5)
     console.log(enemy);
     console.log(enemies);
     //create player and assign to GLOBAL var
     player = game.add.sprite(game.world.randomX, game.world.randomY, 'player');
+    // player.scale.setTo(0.5, 0.5)
     console.log(player);
     //enable physics and world boundaries
     game.physics.arcade.enable(player);
@@ -41,11 +45,30 @@ $('document').ready(function() {
     //uncomment below to test prevent scrolling method
     game.input.keyboard.addKeyCapture(control);
 
+    //create timer to keep track of score
+    timer = game.time.create(false);
+    timer.loop(1000, updateScore, this);
+    timer.start();
+
+    function updateScore() {
+      score++;
+    }
   }
 
   function update() {
     //check for collision
     game.physics.arcade.collide(player, enemies);
+    //callback when collision happens
+    game.physics.arcade.overlap(player, enemies, collideHandler, null, this);
+    //collided function to kill player - note that with overlap callback functions, the sprite individual will always get passed in as the 1st parameter, while the child of the sprite group will get passed in as the 2nd parameter
+    function collideHandler(player, enemy) {
+      console.log('when stars collide')
+      player.kill();
+      enemy.kill();
+      //IMPLEMENT insert update scoreboard function here?
+      timer.stop();
+    }
+
     //player control logic - IMPLEMENT variable speed via function (spacebar acceleration?)
     player.body.velocity.x = 0, player.body.velocity.y = 0;
     if (control.left.isDown) {
@@ -60,9 +83,11 @@ $('document').ready(function() {
     }
 
     //check for angle between enemy and player & apply to directional velocity of enemy to simulate tracking IMPLEMENT multiple enemy tracking
-    var radians = game.physics.arcade.angleBetween(enemies.children[0], player);
-    var degrees = (180 / Math.PI) * radians;
+    var degrees = (180 / Math.PI) * game.physics.arcade.angleBetween(enemies.children[0], player);
     game.physics.arcade.velocityFromAngle(degrees, 100, enemies.children[0].body.velocity);
+  }
 
+  function render() {
+    game.debug.text('Survived for ' + score + ' seconds', 32, 32)
   }
 });
